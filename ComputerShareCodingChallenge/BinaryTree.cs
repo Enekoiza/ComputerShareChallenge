7abd627a6d1a89fdc3484e73e407a55b6b576939
enekoiza <eneko.izaguirre@outlook.com>
2022-07-12 23:31:10 +0100

    Added the construction of the tree

--- ComputerShareCodingChallenge/BinaryTree.cs
+++ ComputerShareCodingChallenge/BinaryTree.cs
@@ -7,7 +7,7 @@ namespace ComputerShareCodingChallenge
 
         public BinaryTree()
 		{
-			root = null;
+            root = new Node();
 		}
 
 		public Node Root
@@ -20,11 +20,42 @@ namespace ComputerShareCodingChallenge
 		public void createTree(string filePath)
         {
 
+
+
+            Node temp = new Node();
+            Node current = new Node();
+
             string line = "";
+
             using (var reader = new System.IO.StreamReader(filePath))
             {
                 while ((line = reader.ReadLine()) != null)
                 {
+                    List<string> dictionary = line.Split(" ").ToList<string>();
+                    current = Root;
+                    foreach (char sign in dictionary[1])
+                    {
+                        if(sign == '.')
+                        {
+                            if(current.LeftNode == null)
+                            {
+                                current.LeftNode = new Node();
+                            }
+                            current = current.LeftNode;
+                        }
+                        if (sign == '-')
+                        {
+                            if (current.RightNode == null)
+                            {
+                                current.RightNode = new Node();
+                            }
+                            current = current.RightNode;
+                        }
+
+                    }
+
+                    current.NodeValue = char.Parse(dictionary[0]);
+
 
                 }
             }
